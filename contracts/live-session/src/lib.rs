306573ff3f846b1de5fd5306429e461d1a8a1ed0
hpapier <hpapier@student.42.fr>
2024-10-26 22:12:20 +0100

    fix: store token into contract

--- contracts/live-session/src/lib.rs
+++ contracts/live-session/src/lib.rs
@@ -1,5 +1,13 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Env, String};
+use soroban_sdk::{contract, contractimpl, contracttype, token, Address, Env, String};
+
+#[contracttype]
+#[derive(PartialEq)]
+pub enum Status {
+    Pending,
+    Success,
+    Fail,
+}
 
 #[contracttype]
 pub enum DataKey {
@@ -13,11 +21,16 @@ pub struct LiveSessionContract;
 #[contractimpl]
 impl LiveSessionContract {
     /// Create a live session and hold the required balance.
-    pub fn create(env: Env, session_id: String, price: String) -> bool {
-        // TODO: Verify the auth_token that is passed in the request, only the auth_token from an authoritative source should be accepted.
+    pub fn deposit(env: Env, session_id: String, price: i128, buyer: Address, token: Address, seller: Address) -> bool {
+
+        // Verify the auth_token that is passed in the request, only the auth_token from an authoritative source should be accepted.
         // TODO: Fetch the amount of tokens from the consumer.
         // TODO: Ensure the consumer has enough tokens to create the live session.
-        // TODO: Transfer and hold the tokens in the contract.
+        buyer.require_auth();
+
+        
+        // Transfer and hold the tokens in the contract.
+        token::Client::new(&env, &token).transfer(&buyer, &env.current_contract_address(), &price);
 
         // Store the balance for the live session.
         env
@@ -26,19 +39,30 @@ impl LiveSessionContract {
             .set(&DataKey::Balance(session_id.clone()), &price);
         
         // TODO: Set the status of the live session to ACTIVE.
+        env
+            .storage()
+            .instance()
+            .set(&DataKey::Status(session_id.clone()), &Status::Pending);
 
         // Ensure the live session has been created.
-        let s: String = env
+        let stored_price: i128 = env
             .storage()
             .instance()
             .get(&DataKey::Balance(session_id.clone()))
-            .unwrap_or(String::from_str(&env, "0"));
+            .unwrap_or(-1);
+
+        // Ensure the status of the live session is ACTIVE.
+        let stored_status: Status = env
+            .storage()
+            .instance()
+            .get(&DataKey::Status(session_id.clone()))
+            .unwrap_or(Status::Fail);
 
         // Return true if everything went well.
-        // TODO: Check if:
+        // Check if:
         // -- The balance is set to the price.
         // -- The status is set to ACTIVE.
-        s == price
+        stored_price == price && stored_status == Status::Pending
     }
 }
 
--- contracts/live-session/src/test.rs
+++ contracts/live-session/src/test.rs
@@ -1,17 +1,165 @@
+
+#![cfg(test)]
+extern crate std;
+
 use crate::{LiveSessionContract, LiveSessionContractClient};
-use soroban_sdk::{Env, String};
+// use soroban_sdk::{testutils::Address as _, token::{self, TokenClient}, xdr::{Asset, ContractIdPreimage, FromXdr, Limits, WriteXdr}, Address, Bytes, Env, String};
+// use token::StellarAssetClient as TokenAdminClient;
+
+use super::*;
+use soroban_sdk::testutils::{Address as _, AuthorizedFunction, AuthorizedInvocation, Ledger};
+use soroban_sdk::{symbol_short, token, Address, Env, IntoVal};
+use token::Client as TokenClient;
+use token::StellarAssetClient as TokenAdminClient;
+
+fn create_token_contract<'a>(e: &Env, admin: &Address) -> (TokenClient<'a>, TokenAdminClient<'a>) {
+    let sac = e.register_stellar_asset_contract_v2(admin.clone());
+    (
+        token::Client::new(e, &sac.address()),
+        token::StellarAssetClient::new(e, &sac.address()),
+    )
+}
+
+fn create_live_session_contract<'a>(e: &Env) -> LiveSessionContractClient<'a> {
+    LiveSessionContractClient::new(e, &e.register_contract(None, LiveSessionContract {}))
+}
+
+struct ClaimableBalanceTest<'a> {
+    env: Env,
+    deposit_address: Address,
+    claim_addresses: [Address; 3],
+    token: TokenClient<'a>,
+    contract: LiveSessionContractClient<'a>,
+}
+
+impl<'a> ClaimableBalanceTest<'a> {
+    fn setup() -> Self {
+        let env = Env::default();
+        env.mock_all_auths();
+
+        env.ledger().with_mut(|li| {
+            li.timestamp = 12345;
+        });
+
+        let deposit_address = Address::generate(&env);
+
+        let claim_addresses = [
+            Address::generate(&env),
+            Address::generate(&env),
+            Address::generate(&env),
+        ];
+
+        let token_admin = Address::generate(&env);
+
+        let (token, token_admin_client) = create_token_contract(&env, &token_admin);
+        token_admin_client.mint(&deposit_address, &1000);
+
+        let contract = create_live_session_contract(&env);
+        ClaimableBalanceTest {
+            env,
+            deposit_address,
+            claim_addresses,
+            token,
+            contract,
+        }
+    }
+}
 
 #[test]
-fn it_creates_a_live_session() {
-    let env = Env::default();
-    let contract_id = env.register_contract(None, LiveSessionContract);
-    let client = LiveSessionContractClient::new(&env, &contract_id);
+fn it_deposits_token_into_contract() {
+    let test = ClaimableBalanceTest::setup();
+    test.contract.deposit(
+        &String::from_str(&test.env, "random_id"),
+        &800,
+        &test.deposit_address,
+        &test.token.address,
+        &test.claim_addresses[0],
+    );
+
+    let expected_auths_0 = [(
+        test.deposit_address.clone(),
+        AuthorizedInvocation {
+            function: AuthorizedFunction::Contract((
+                test.contract.address.clone(),
+                symbol_short!("deposit"),
+                (
+                    String::from_str(&test.env, "random_id"),
+                    800_i128,
+                    test.deposit_address.clone(),
+                    test.token.address.clone(),
+                    test.claim_addresses[0].clone(),
+                )
+                .into_val(&test.env),
+            )),
+            sub_invocations: std::vec![AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    test.token.address.clone(),
+                    symbol_short!("transfer"),
+                    (
+                        test.deposit_address.clone(),
+                        &test.contract.address,
+                        800_i128,
+                    )
+                        .into_val(&test.env),
+                )),
+                sub_invocations: std::vec![]
+            }]
+        }
+    ),];
 
     assert_eq!(
-        client.create(
-            &String::from_str(&env, "random_id"),
-            &String::from_str(&env, "133")
-        ),
-        true
+        test.env.auths(),
+        expected_auths_0
     );
+
+    assert_eq!(test.token.balance(&test.deposit_address), 200);
+    assert_eq!(test.token.balance(&test.contract.address), 800);
+    assert_eq!(test.token.balance(&test.claim_addresses[0]), 0);
+    assert_eq!(test.token.balance(&test.claim_addresses[1]), 0);
+    assert_eq!(test.token.balance(&test.claim_addresses[2]), 0);
+
+    test.contract.deposit(
+        &String::from_str(&test.env, "random_id_1"),
+        &200,
+        &test.deposit_address,
+        &test.token.address,
+        &test.claim_addresses[0],
+    );
+
+    let expected_auths_1 = [(
+        test.deposit_address.clone(),
+        AuthorizedInvocation {
+            function: AuthorizedFunction::Contract((
+                test.contract.address.clone(),
+                symbol_short!("deposit"),
+                (
+                    String::from_str(&test.env, "random_id_1"),
+                    800_i128,
+                    test.deposit_address.clone(),
+                    test.token.address.clone(),
+                    test.claim_addresses[0].clone(),
+                )
+                .into_val(&test.env),
+            )),
+            sub_invocations: std::vec![AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    test.token.address.clone(),
+                    symbol_short!("transfer"),
+                    (
+                        test.deposit_address.clone(),
+                        &test.contract.address,
+                        800_i128,
+                    )
+                        .into_val(&test.env),
+                )),
+                sub_invocations: std::vec![]
+            }]
+        }
+    )];
+
+    assert_eq!(test.token.balance(&test.deposit_address), 0);
+    assert_eq!(test.token.balance(&test.contract.address), 1000);
+    assert_eq!(test.token.balance(&test.claim_addresses[0]), 0);
+    assert_eq!(test.token.balance(&test.claim_addresses[1]), 0);
+    assert_eq!(test.token.balance(&test.claim_addresses[2]), 0);
 }
